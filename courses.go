7c43d078331428b4d13e8b3217777e9a0cb4315b
grant eaton <grant eaton>
2016-12-31 01:40:33 -0500

    parsing json file, needs help with parsing inner meets data. check comments for details.

--- courses.go
+++ courses.go
@@ -21,6 +21,46 @@ func importCourses() {
 		if err != nil {
 			log.Fatal("Could not parse campus")
 		}
-		fmt.Println(campus)
+		credits, err := jsonparser.GetString(value, "credits")
+		if err != nil {
+			log.Fatal("Could not parse credits")
+		}
+		id, err := jsonparser.GetString(value, "id")
+		if err != nil {
+			log.Fatal("Could not parse id")
+		}
+		number, err := jsonparser.GetString(value, "number")
+		if err != nil {
+			log.Fatal("Could not parse number")
+		}
+		section, err := jsonparser.GetString(value, "section")
+		if err != nil {
+			log.Fatal("Could not parse section")
+		}
+		subject, err := jsonparser.GetString(value, "subject")
+		if err != nil {
+			log.Fatal("Could not parse subject")
+		}
+		title, err := jsonparser.GetString(value, "title")
+		if err != nil {
+			log.Fatal("Could not parse title")
+		}
+
+		fmt.Println("campus: " + campus, "credits: " + credits, "id: " + id,
+			" number: "+ number, " section: " + section, " subject: " + subject, " title: " + title)
+
+		//below line will properly get "meets". I tried inputting
+		//[]byte(meets) instead of "data", but that didnt work either
+		//meets, err := jsonparser.GetString(value, "meets")
+
+		//should iterate through all the meets and print them. doesnt work, obviously.
+		//I think it has to do with the fact its seaching all of data for meets
+		jsonparser.ArrayEach(data, func(value []byte, dataType jsonparser.ValueType, offset int, err error) {
+
+			fmt.Println("day: ")
+			fmt.Println(jsonparser.Get(value, "day"))
+		},"meets")
+
 	})
+
 }
