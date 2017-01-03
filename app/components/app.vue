cf0d606fc2e850ff3476d90ad20ace21cd8cc7ce
Luke <lartnak@gmail.com>
2017-01-03 13:47:47 -0500

    format front-end structure

---
+++ app/components/app.vue
@@ -0,0 +1,28 @@
+<template>
+
+  <h1>Schedule Buddy</h1>
+
+</template>
+
+<script>
+
+  export default {
+    name: 'app',
+    data: () => ({
+
+    })
+  }
+
+</script>
+
+<style>
+
+  h1 {
+    font-family: 'Avenir', Helvetica, Arial, sans-serif;
+    text-align: center;
+    color: #2c3e50;
+    margin-top: 60px;
+    font-weight: normal;
+  }
+
+</style>
--- webpack.config.js
+++ webpack.config.js
@@ -2,7 +2,7 @@ var path = require('path')
 var webpack = require('webpack')
 
 module.exports = {
-  entry: './app/main.js',
+  entry: './app/index.js',
   output: {
     path: path.resolve(__dirname, './dist'),
     publicPath: '/dist/',
@@ -15,13 +15,8 @@ module.exports = {
         loader: 'vue-loader',
         options: {
           loaders: {
-            // Since sass-loader (weirdly) has SCSS as its default parse mode, we map
-            // the "scss" and "sass" values for the lang attribute to the right configs here.
-            // other preprocessors should work out of the box, no loader config like this nessessary.
             'scss': 'vue-style-loader!css-loader!sass-loader',
-            'sass': 'vue-style-loader!css-loader!sass-loader?indentedSyntax'
           }
-          // other vue-loader options go here
         }
       },
       {
