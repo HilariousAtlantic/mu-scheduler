af234514a6cea8a54ccae2b8706ec4afdc395cc3
Luke <lartnak@gmail.com>
2017-01-03 21:07:26 -0500

    fetch the list of courses for the course selection

--- app/components/app.vue
+++ app/components/app.vue
@@ -1,23 +1,51 @@
 <template>
 
-  <h1>Schedule Buddy</h1>
+  <div class="app">
+
+    <h1>Schedule Buddy</h1>
+
+    <h2>Select courses you would like to take this semester</h2>
+
+    <ul>
+
+      <li v-for="course in courses">{{course.subject}} {{course.number}} - {{course.name}}</li>
+
+    </ul>
+
+  </div>
 
 </template>
 
 <script>
 
-  export default {
-    name: 'app',
-    data: () => ({
+  import axios from 'axios';
 
-    })
+  export default {
+
+    name: 'app',
+
+    data() {
+
+      return {
+        courses: []
+      }
+
+    },
+
+    mounted() {
+
+      axios.get('/courses')
+        .then(response => this.courses = response.data.slice(0, 30))
+
+    }
+
   }
 
 </script>
 
 <style>
 
-  h1 {
+  h1, h2 {
     font-family: 'Avenir', Helvetica, Arial, sans-serif;
     text-align: center;
     color: #2c3e50;
