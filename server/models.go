2896c5f3eb6b805909371314eb8498fd5a70d067
Grant <garfield@iMac.local>
2017-01-06 20:54:15 -0500

    added meets struct, working on function to read them into db

--- server/models.go
+++ server/models.go
@@ -7,6 +7,15 @@ type Course struct {
 	Number  string `json:"number"`
 	Credits string `json:"credits"`
 }
+type Meet struct {
+	Days       string `json:"days"`
+	Start_time int    `json:"start_time"`
+	End_time   int    `json:"end_time"`
+	Instructor string `json:"instructor"`
+	Location   string `json:"location"`
+	Start_date string `json:"start_date"`
+	End_date   string `json:"end_date"`
+}
 
 func getCoursesFromJSON(parsed *parsedCoursesJSON) []*Course {
 	courses := make([]*Course, 0)
@@ -22,3 +31,16 @@ func getCoursesFromJSON(parsed *parsedCoursesJSON) []*Course {
 	}
 	return courses
 }
+
+//un-finished funciton, coming back later.
+/*
+func getMeetsFropmJSON(parsed *parsedCoursesJSON) []*Meet {
+	meets := make([]*Meet, 0)
+	for _, meet := range *parsed {
+		m := &Meet{
+			fmt.Printf(m)
+		}
+
+	}
+}
+*/
