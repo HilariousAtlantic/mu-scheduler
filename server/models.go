97c4101bd283493b066c2ffd12bd2bded5c04c21
Luke <lartnak@gmail.com>
2017-01-09 21:56:22 -0500

    fix formatting maybe

--- server/models.go
+++ server/models.go
@@ -37,48 +37,46 @@ type Section struct {
 /*
 
 type Term struct {
-	ID     			int    	`json:"id"`
-	Season 			string 	`json:"season"`
-	Year   			string 	`json:"year"`
-	Name   			string 	`json:"name"`
+	ID		int			`json:"id"`
+	Name	string	`json:"name"`
 }
 
 type Course struct {
-	ID       		int    	`json:"id"`
-	TermID 			int    	`json:"term_id"`
-	Title     	string 	`json:"title"`
-	Subject  		string 	`json:"subject"`
-	Number   		string 	`json:"number"`
-	Credits  		string 	`json:"credits"`
+	ID				int			`json:"id"`
+	TermID		int			`json:"term_id"`
+	Title			string	`json:"title"`
+	Subject		string	`json:"subject"`
+	Number		string	`json:"number"`
+	Credits		string	`json:"credits"`
 }
 
 type Section struct {
-	ID      		int    	`json:"id"`
-	CourseID 		int			`json:"course_id"`
+	ID					int			`json:"id"`
+	CourseID		int			`json:"course_id"`
 	CRN					string	`json:"crn"`
-	Name 				string 	`json:"name"`
-	Campus  		string 	`json:"campus"`
+	Name				string	`json:"name"`
+	Campus			string	`json:"campus"`
 }
 
 type Meet struct {
-	ID       		int    	`json:"id"`
-	SectionID 	int    	`json:"section_id"`
-	Days       	string 	`json:"days"`
-	StartTime 	string 	`json:"start_time"`
-	EndTime   	string 	`json:"end_time"`
-	Instructor 	string 	`json:"instructor"`
-	Location   	string 	`json:"location"`
-	StartDate 	string 	`json:"start_date"`
-	EndDate   	string 	`json:"end_date"`
+	ID					int			`json:"id"`
+	SectionID		int			`json:"section_id"`
+	Days				string	`json:"days"`
+	StartTime		string	`json:"start_time"`
+	EndTime			string	`json:"end_time"`
+	Instructor	string	`json:"instructor"`
+	Location		string	`json:"location"`
+	StartDate		string	`json:"start_date"`
+	EndDate			string	`json:"end_date"`
 }
 
 type Test struct {
-	ID       		int    	`json:"id"`
-	SectionID 	int    	`json:"section_id"`
-	StartTime 	string 	`json:"start_time"`
-	EndTime   	string 	`json:"end_time"`
-	Location   	string 	`json:"location"`
-	Date 				string 	`json:"date"`
+	ID					int			`json:"id"`
+	SectionID		int			`json:"section_id"`
+	StartTime		string	`json:"start_time"`
+	EndTime			string	`json:"end_time"`
+	Location		string	`json:"location"`
+	Date				string	`json:"date"`
 }
 
 */
