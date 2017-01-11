9064b976d11c66cff0cab3fa01d75443f95316da
Grant <garfield@iMac.local>
2017-01-10 19:40:20 -0500

    updated structs and getTableFromJson functions to support new json format, just db.go to go.

--- server/models.go
+++ server/models.go
@@ -1,114 +1,102 @@
 package main
 
-type Course struct {
-	ID       int    `json:"id"`
-	Semester int    `json:"semester_id"`
-	Name     string `json:"name"`
-	Subject  string `json:"subject"`
-	Number   string `json:"number"`
-	Credits  string `json:"credits"`
+type Term struct {
+	ID   int    `json:"id"`
+	Name string `json:"name"`
 }
 
-type Semester struct {
-	ID     int    `json:"id"`
-	Season string `json:"Season"`
-	Year   int    `json:"Year"`
-	Name   string `json:"name"`
+type Course struct {
+	ID      int    `json:"id"`
+	TermID  int    `json:"term_id"`
+	Subject string `json:"subject"`
+	Number  string `json:"number"`
+	Title   string `json:"title"`
+	Credits string `json:"credits"`
+}
+
+type Section struct {
+	ID       int    `json:"id"`
+	CourseID int    `json:"course_id"`
+	CRN      string `json:"crn"`
+	Name     string `json:"name"`
+	Campus   string `json:"campus"`
 }
 
 type Meet struct {
 	ID         int    `json:"id"`
-	Section_id string `json:"days"`
+	SectionID  int    `json:"section_id"`
 	Days       string `json:"days"`
-	Start_time int    `json:"start_time"`
-	End_time   int    `json:"end_time"`
+	StartTime  string `json:"start_time"`
+	EndTime    string `json:"end_time"`
 	Instructor string `json:"instructor"`
 	Location   string `json:"location"`
-	Start_date string `json:"start_date"`
-	End_date   string `json:"end_date"`
-}
-
-type Section struct {
-	ID      int    `json:"id"`
-	Section string `json:"section"`
-	Campus  string `json:"campus"`
-}
-
-/*
-
-type Term struct {
-	ID    int     `json:"id"`
-	Name  string  `json:"name"`
-}
-
-type Course struct {
-	ID        int     `json:"id"`
-	TermID    int     `json:"term_id"`
-	Title     string  `json:"title"`
-	Subject   string  `json:"subject"`
-	Number    string  `json:"number"`
-	Credits   string  `json:"credits"`
-}
-
-type Section struct {
-	ID        int     `json:"id"`
-	CourseID  int     `json:"course_id"`
-	CRN       string  `json:"crn"`
-	Name      string  `json:"name"`
-	Campus    string  `json:"campus"`
-}
-
-type Meet struct {
-	ID          int     `json:"id"`
-	SectionID   int     `json:"section_id"`
-	Days        string  `json:"days"`
-	StartTime   string  `json:"start_time"`
-	EndTime     string  `json:"end_time"`
-	Instructor  string  `json:"instructor"`
-	Location    string  `json:"location"`
-	StartDate   string  `json:"start_date"`
-	EndDate     string  `json:"end_date"`
+	StartDate  string `json:"start_date"`
+	EndDate    string `json:"end_date"`
 }
 
 type Test struct {
-	ID          int     `json:"id"`
-	SectionID   int     `json:"section_id"`
-	StartTime   string  `json:"start_time"`
-	EndTime     string  `json:"end_time"`
-	Location    string  `json:"location"`
-	Date        string  `json:"date"`
+	ID        int    `json:"id"`
+	SectionID int    `json:"section_id"`
+	Date      string `json:"date"`
+	Location  string `json:"location"`
+	StartTime string `json:"start_time"`
+	EndTime   string `json:"end_time"`
 }
-
-*/
 
 func getMeetsFromJSON(parsed *parsedCoursesJSON) []*Meet {
 	meets := make([]*Meet, 0)
 	for _, course := range *parsed {
-		for _, meet := range course.Meets {
-			m := &Meet{
-				ID:         -1,
-				Section_id: course.ID,
-				Days:       meet.Days,
-				Start_time: -1,
-				End_time:   -1,
-				Instructor: meet.Instructor,
-				Location:   meet.Location,
-				Start_date: "Not set in import.go",
-				End_date:   "Not set in import.go",
+		for _, section := range coure.Sections {
+			for _, meet := range course.Meets {
+				m := &Meet{
+					ID:         -1,
+					SectionID:  section.ID,
+					Days:       meet.Days,
+					StartTime:  meet.StartTime,
+					EndTime:    meet.EndTime,
+					Instructor: meet.Instructor,
+					Location:   meet.Location,
+					StartDate:  meet.StartDate,
+					EndDate:    meet.EndDate,
+				}
+				meets = append(meets, m)
 			}
-			meets = append(meets, m)
 		}
 	}
 	return meets
 }
 
+func getTestsFromJSON(parsed *parsedCoursesJSON) []*Test {
+	tests := make([]*Test, 0)
+	for _, course := range *parsed {
+		for _, section := range coure.Sections {
+			for _, test := range course.Test {
+				t := &test{
+					ID:        -1,
+					SectionID: section.ID,
+					Date:      test.Date,
+					StartTime: test.StartTime,
+					EndTime:   test.EndTime,
+					Location:  meet.Location,
+				}
+				test = append(test, t)
+			}
+		}
+	}
+	return test
+}
+
 func getSectionsFromJSON(parsed *parsedCoursesJSON) []*Section {
 	sections := make([]*Section, 0)
-	for _, section := range *parsed {
-		s := &Section{
-			ID:      -1,
-			Section: section.Section,
-			Campus:  section.Campus,
+	for _, course := range *parsed {
+		for _, section := range course.Sections {
+			s := &Section{
+				ID:       -1,
+				CourseID: course.ID,
+				CRN:      section.CRN,
+				Name:     section.Name,
+				Campus:   section.Campus,
+			}
 		}
 		sections = append(sections, s)
 	}
@@ -119,12 +107,12 @@ func getCoursesFromJSON(parsed *parsedCoursesJSON) []*Course {
 	courses := make([]*Course, 0)
 	for _, course := range *parsed {
 		c := &Course{
-			ID:       -1,
-			Semester: 3,
-			Name:     course.Title,
-			Subject:  course.Subject,
-			Number:   course.Number,
-			Credits:  course.Credits,
+			ID:      -1,
+			TermID:  "spring 2017",
+			Subject: course.Subject,
+			Number:  course.Number,
+			Title:   course.Title,
+			Credits: course.Credits,
 		}
 		courses = append(courses, c)
 	}
