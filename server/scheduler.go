23df5ea5cb044af1f8615e9c42708fb7c182c296
Grant <Grantman100@gmail.com>
2017-01-12 14:16:20 -0500

    main.go looks gross. ignore it. for testing. removed errors in scheudler.go

--- server/scheduler.go
+++ server/scheduler.go
@@ -1,33 +1,35 @@
 package main
 
-
 //without comments
 //goodSchedules := make([[]Section]Course,0)
-findGoodSchedules(courses *[]Course, selectedSections *[]Section){
+/*
+func findGoodSchedules(courses *[]Course, selectedSections *[]Section) {
 
-    if len(selectedSections) == len(courses) {
-        append(goodSchedules, selectedSections)
-        return
-    }
-    for _, course := range courses{
+	if len(*selectedSections) == len(*courses) {
+		GoodSchedules = append(GoodSchedules, *selectedSections)
+		return
+	}
+SKIPCOURSE:
+	for _, course := range *courses {
 
-        for _, selectedSection := range selectedSections{
-            if selectedSection.courseID == course.ID{
-                continue
-            }
-        }
-        for _, section := range course{
-            for _, selectedSection := range selectedSections{
+		for _, selectedSection := range *selectedSections {
+			if selectedSection.CourseID == course.ID {
+				continue SKIPCOURSE
+			}
+		}
+		for _, section := range getSectionsFromCourse(*course) {
+			for _, selectedSection := range *selectedSections {
 
-                if !(section.startTime > selectedSection.startTime
-                    && section.startTime < selectedSection.endTime ||
-                    selectedSection.startTime > section.startTime
-                    && selectedSection.startTime < section.startTime){
-                        append(selectedSections, section)
-                        findGoodSchedules(courses,selectedSections)
-                    }
-            }
-            return
-        }
-    }
+				if !(section.startTime > selectedSection.startTime &&
+					section.startTime < selectedSection.endTime ||
+					selectedSection.startTime > section.startTime &&
+						selectedSection.startTime < section.startTime) {
+					*selectedSections = append(*selectedSections, section)
+					findGoodSchedules(courses, selectedSections)
+				}
+			}
+			return
+		}
+	}
 }
+*/
