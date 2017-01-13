dfc6dc8d5b5a4f69fdd73ba88ee94fdbb5f87ec7
Luke <lartnak@gmail.com>
2017-01-12 19:01:27 -0500

    add mock filters for the schedule selection

--- app/pages/schedule-selection.vue
+++ app/pages/schedule-selection.vue
@@ -8,6 +8,54 @@
 
         <h3>Schedule Selection</h3>
 
+        <div class="filter-group">
+
+          <h4>Time Filters</h4>
+
+          <button><i class="fa fa-plus"></i> Filter</button>
+
+        </div>
+
+        <div class="filter">
+
+          <input type="checkbox">
+
+          <span>I want to start after 10:00 AM on Monday, Wednesday, and Friday</span>
+
+        </div>
+
+        <div class="filter">
+
+          <input type="checkbox">
+
+          <span>I want to finish before 4:00 PM on Tuesday and Thursday</span>
+
+        </div>
+
+        <div class="filter-group">
+
+          <h4>Class Filters</h4>
+
+          <button><i class="fa fa-plus"></i> Filter</button>
+
+        </div>
+
+        <div class="filter">
+
+          <input type="checkbox">
+
+          <span>I want less than 3 classes on Monday, Wednesday, and Friday</span>
+
+        </div>
+
+        <div class="filter">
+
+          <input type="checkbox">
+
+          <span>I want more than 1 class on Tuesday and Thursday</span>
+
+        </div>
+
       </div>
 
     </sidebar>
@@ -43,7 +91,7 @@
 
   .sidebar {
 
-    width: 300px;
+    width: 350px;
 
   }
 
@@ -59,6 +107,47 @@
     h3 {
 
       margin-top: 0;
+      margin-bottom: 15px;
+
+    }
+
+    h4 {
+
+      display: inline-block;
+      margin: 0;
+
+    }
+
+    .filter-group {
+
+      margin: 15px 0;
+
+      button {
+
+        float: right;
+        border: none;
+        background: transparent;
+
+      }
+
+    }
+
+    .filter {
+
+      display: flex;
+      margin-bottom: 10px;
+
+      input {
+
+        margin-right: 5px;
+
+      }
+
+      span {
+
+        flex: 1;
+
+      }
 
     }
 
