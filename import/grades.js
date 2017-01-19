25947b3d61ed126330778d5a2d16a91b85d17931
Brennan Hoeting <brennan.hoeting@gmail.com>
2017-01-19 12:41:38 -0500

    fix grades import script, add csv

--- import/grades.js
+++ import/grades.js
@@ -1,60 +1,72 @@
 const fs = require('fs');
 const util = require('util');
+const sleep = require('sleep');
 const readline = require('readline');
 const rq = require('request-promise-native');
 
-//const http://grdist.miamioh.edu/php/getClasses.php?dept=%v&num=%v&inst=&from=%v&to=%v&iid=-1&did=%v&sem=&loc=O
-
 let writer = fs.createWriteStream('./grades.csv');
+
+let courses = [];
+let coursesSet = new Set();
+
+let allGPAs = [];
+
+let semesters = {
+  "10": "Fall",
+  "15": "Winter",
+  "20": "Spring",
+  "30": "Summer"
+};
 
 let reader = readline.createInterface({
   input: fs.createReadStream('./courses.csv', {encoding: 'UTF-8'})
 });
 
-let allGrades = [];
-
-let courses = new Set();
-let subjects = new Map();
-
-
 reader.on('line', line => {
   let [section, term, crn, subject, number] = line.split(',');
 
-  if (courses.has(subject + number)) return;
-  courses.add(subject + number);
-
-  fetchDepartmentId(subject)
-    .then(id => console.log(id))
-    //.then(id => fetchGPAs)
-    //.then(() => [...allGrades, ...grades])
-    .catch((err) => {
-      util.log(err);
-    });
+  if (coursesSet.has(subject + number)) return;
+  coursesSet.add(subject + number);
 });
 
-function fetchGPAs(subject, number) {
+reader.on('close', () => {
+  coursesSet.forEach(course => courses.push({
+    subject: course.substr(0, 3),
+    number: course.substr(3)
+  }));
 
+  fetchGPAs(courses.pop());
+});
+
+function fetchGPAs(course) {
+  let url = `http://grdist.miamioh.edu/php/getClasses.php?sem=&loc=O&inst=&from=2000&to=2016&iid=-1&did=-1&dept=${course.subject}&num=${course.number}`;
+
+  console.log(`Fetching grades for ${course.subject + course.number} from ${url}`);
+
+  rq({url: url, json: true})
+    .then(GPAs  => {
+      saveGPAs(GPAs);
+      if (courses.length > 0) {
+        fetchGPAs(courses.pop());
+      }
+    })
+    .catch(err => {
+      util.log(`Request for ${subject+number} grades failed. ${err}`);
+    })
 }
 
-function fetchDepartmentId(subject) {
-  return new Promise((resolve, reject) => {
-    if (subjects.has(subject)) {
-       resolve(subjects.get(subject));
-    } else {
-      rq({url: `http://grdist.miamioh.edu/php/getDID.php?dept=${subject}`, json: true})
-        .then(body => JSON.parse(body))
-        .then(data => {
-          if (data.length > 0 && data[0].did > -1) {
-            subjects.set(subject, data[0].did);
-            resolve(data);
-          } else {
-            subjects.set(subject, null);
-            reject(`Failed to fetch department ID for ${subject}.  Response: ${data}`);
-          }
-        })
-        .catch(err => {
-          reject(`Request for ${subject} department ID failed. ${err}`);
-        });
-    }
+function saveGPAs(GPAs) {
+  GPAs.forEach(gpa => {
+    if (gpa.campus != "O") return;
+
+    writer.write([
+      gpa.avggpa,
+      gpa.name,
+      gpa.NameShort,
+      gpa.number,
+      gpa.Year,
+      semesters[gpa.Semester],
+      gpa.Section,
+    ].join(',') + '\n');
   });
 }
