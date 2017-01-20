285504bbfae376661b18c162d514e66ab5bf17f3
Luke <lartnak@gmail.com>
2017-01-19 20:37:52 -0500

    filter schedules using a default test

--- app/store/state.js
+++ app/store/state.js
@@ -12,13 +12,13 @@ export default {
     schedulesIndex: 0,
     scheduleFilters: [
 
-      {id: 0, type: 'time', options: {operator: 'Start After', time: 600, days: ['M', 'W', 'F']}, active: true},
+      {id: 0, type: 'time', options: {operator: 'Start After', time: 600, days: ['M', 'W', 'F']}, active: true, test(schedule) {return true}},
 
-      {id: 1, type: 'time', options: {operator: 'End Before', time: 1080, days: ['T', 'R']}, active: false},
+      {id: 1, type: 'time', options: {operator: 'End Before', time: 1080, days: ['T', 'R']}, active: false, test(schedule) {return true}},
 
-      {id: 2, type: 'class', options: {operator: 'At Most', amount: 2, days: ['M']}, active: false},
+      {id: 2, type: 'class', options: {operator: 'At Most', amount: 2, days: ['M']}, active: false, test(schedule) {return true}},
 
-      {id: 3, type: 'class', options: {operator: 'Exactly', amount: 3, days: ['T', 'R']}, active: true}
+      {id: 3, type: 'class', options: {operator: 'Exactly', amount: 3, days: ['T', 'R']}, active: true, test(schedule) {return true}}
 
     ],
 
