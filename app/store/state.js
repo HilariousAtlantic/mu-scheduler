deecc6f5f6568ef6ec517d1c0fd07b051c372da1
Luke <lartnak@gmail.com>
2017-01-20 00:31:11 -0500

    fix placeholder filter that made it hard to test

--- app/store/state.js
+++ app/store/state.js
@@ -13,7 +13,7 @@ export default {
 
       {id: 0, type: 'time', options: {operator: 'Start After', time: 600, days: ['M', 'W', 'F']}, active: false, test: () => true},
 
-      {id: 1, type: 'time', options: {operator: 'End Before', time: 1080, days: ['T', 'R']}, active: false, test: () => true},
+      {id: 1, type: 'time', options: {operator: 'Finish Before', time: 1080, days: ['T', 'R']}, active: false, test: () => true},
 
       {id: 2, type: 'class', options: {operator: 'At Most', amount: 2, days: ['M']}, active: false, test: () => true},
 
