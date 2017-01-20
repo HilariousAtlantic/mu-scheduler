3451ff5b5014e02d8de7fe294e764ca1f6e38ba1
Luke <lartnak@gmail.com>
2017-01-19 19:43:48 -0500

    add functionality to class filter

---
+++ app/components/filter/class-filter-options.vue
@@ -0,0 +1,75 @@
+<template>
+
+  <div class="class-filter-options">
+
+    <dropdown
+      :options="['At Least', 'At Most', 'Exactly']"
+      :defaultOption="options.operator"
+      @select="onOperatorChange"
+    ></dropdown>
+
+    <number-input
+      :step="1"
+      :defaultNumber="options.amount"
+      @change="onAmountChange"
+    ></number-input>
+
+    <days-input
+      :days="['M', 'T', 'W', 'R', 'F', 'S']"
+      :defaultDays="options.days"
+      @change="onDaysChange"
+    ></days-input>
+
+  </div>
+
+</template>
+
+<script>
+
+  import Dropdown from '../editor/dropdown.vue';
+  import NumberInput from '../editor/number-input.vue';
+  import DaysInput from '../editor/days-input.vue';
+
+  export default {
+
+    name: 'class-filter-options',
+
+    props: ['options'],
+
+    components: {Dropdown, NumberInput, DaysInput},
+
+    methods: {
+
+      onOperatorChange(operator) {
+
+        this.$emit('change', {operator});
+
+      },
+
+      onAmountChange(amount) {
+
+        this.$emit('change', {amount});
+
+      },
+
+      onDaysChange(days) {
+
+        this.$emit('change', {days});
+
+      }
+
+    }
+
+  }
+
+</script>
+
+<style scoped>
+
+  .class-filter-options {
+
+    display: inline-block;
+
+  }
+
+</style>
--- app/components/filter/class-filter.vue
+++ app/components/filter/class-filter.vue
@@ -2,7 +2,17 @@
 
   <div class="class-filter">
 
-    <schedule-filter :text="text" :active="active"></schedule-filter>
+    <schedule-filter :text="text" :active="active" @edit="toggleEditing" @toggle="toggleActive"></schedule-filter>
+
+    <modal v-if="showEditor">
+
+      <filter-editor :result="previewText" @done="submitChanges" @quit="discardChanges">
+
+        <class-filter-options :options="preview" @change="changePreview"></class-filter-options>
+
+      </filter-editor>
+
+    </modal>
 
   </div>
 
@@ -11,6 +21,11 @@
 <script>
 
   import ScheduleFilter from './schedule-filter.vue';
+  import Modal from '../modal.vue';
+  import FilterEditor from './filter-editor.vue';
+  import ClassFilterOptions from './class-filter-options.vue';
+
+  import {formatDayList} from '../../lib/days';
 
   export default {
 
@@ -18,13 +33,79 @@
 
     props: ['options', 'active'],
 
-    components: {ScheduleFilter},
+    components: {ScheduleFilter, Modal, FilterEditor, ClassFilterOptions},
+
+    data() {
+
+      return {
+
+        showEditor: false,
+
+        preview: Object.assign({}, this.options)
+
+      }
+
+    },
 
     computed: {
 
       text() {
 
-        return 'I want ' + this.options.operator.toLowerCase() + ' ' + this.options.amount + ' classes on ' + this.options.days;
+        let operator = this.options.operator.toLowerCase();
+
+        let amount = this.options.amount;
+
+        let days = formatDayList(this.options.days);
+
+        return 'I want ' + operator + ' ' + amount + ' classes on ' + days;
+
+      },
+
+      previewText() {
+
+        let operator = this.preview.operator.toLowerCase();
+
+        let amount = this.preview.amount;
+
+        let days = formatDayList(this.preview.days);
+
+        return 'I want ' + operator + ' ' + amount + ' classes on ' + days;
+
+      }
+
+    },
+
+    methods: {
+
+      toggleEditing() {
+
+        this.showEditor = !this.showEditor;
+
+      },
+
+      toggleActive() {
+
+        // TODO dispatch a filter change action
+
+      },
+
+      changePreview(changes) {
+
+        Object.assign(this.preview, changes);
+
+      },
+
+      submitChanges() {
+
+        // TODO dispatch a filter change action
+        this.toggleEditing();
+
+      },
+
+      discardChanges() {
+
+        Object.assign(this.preview, this.options);
+        this.toggleEditing();
 
       }
 
