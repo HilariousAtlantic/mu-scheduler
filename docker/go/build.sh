6cd32b5b7959b8c6b3fc6ff53a0113a078adad3d
Brennan Hoeting <brennan.hoeting@gmail.com>
2017-04-21 23:08:54 -0400

    fix derp

--- docker/go/build.sh
+++ docker/go/build.sh
@@ -3,5 +3,6 @@
 go get github.com/labstack/echo
 go get github.com/lib/pq
 go get github.com/labstack/echo/middleware
+go get github.com/jmoiron/sqlx
 go build -o start-server server/*.go
 ./start-server
